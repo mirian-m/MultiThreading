530144cc962412590c8d6961f8247e734e1d1e9e
mirian-m <mirianmaglakelidze7@gmail.com>
2022-08-09 14:54:38 +0400

    Add Multi-threadinga task

--- mirianMaglakelidze#27/ViewController.swift
+++ mirianMaglakelidze#27/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var calculateBtn: UIButton! {
+        didSet {
+            calculateBtn.setTitle("Calculate Prime Numbers", for: .normal)
+        }
+    }
+    @IBOutlet weak var dataPicker: UIDatePicker!
+    let numberRange = 10000
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateBtnAction(_ sender: Any) {
+        DispatchQueue.global(qos: .background).sync { [weak self] in
+            self?.calculateBtn.setTitle("calculating...", for: .normal)
+            self?.calculateBtn.isEnabled = false
+            self?.printPrimeNumber()
+        }
+    }
+    
+    @IBAction func pickerAction(_ sender: Any) {
+        
+    }
+    
+    func printPrimeNumber() {
+        DispatchQueue.global(qos: .background).async {
+            for number in 1...self.numberRange {
+                if self.isnNumberPrime(number) {
+                    print(number)
+                }
+            }
+            DispatchQueue.main.async { [weak self] in
+                self?.calculateBtn.setTitle("Calculate Prime Numbers", for: .normal)
+                self?.calculateBtn.isEnabled = true
+            }
+        }
+    }
+    func isnNumberPrime(_ number: Int) -> Bool {
+        if number == 2 {
+            return true
+        } else if number < 2 {
+            return false
+        } else {
+            for i in 3..<number {
+                if number.isMultiple(of: i) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
 }
 
